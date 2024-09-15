213eda67bb2f9bbc69b3040a72f80ccdb8a02793
Dum <91747868+Dum1121@users.noreply.github.com>
2024-09-15 11:58:49 +0700

    Update CuToiNho.lua

--- CuToiNho.lua
+++ CuToiNho.lua
@@ -1,6 +1,6 @@
 getgenv().rei = {
     ["Key"] = "eLtkmWQRZfghUoJDVRwjdeVmPcjiOXlP",
-    ["MainAccount"] = "Nguaoo3",
+    ["MainAccount"] = "clonethanhhau7",
     ["AccountForMainToFolow"] = false,
     ["MainAccountSetting"] = {
         Units = {"Legion Assassin","Legion Veteran","Blazing Soulmaster","Ace Marksman"},
