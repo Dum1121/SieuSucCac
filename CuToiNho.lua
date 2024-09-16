38a6c746a301b3168b05f4bcd987e74d384bbfb2
Dum <91747868+Dum1121@users.noreply.github.com>
2024-09-16 23:29:25 +0700

    ?

--- CuToiNho.lua
+++ CuToiNho.lua
@@ -14,7 +14,7 @@ getgenv().rei = {
         KickAfterDoneTrade= false,
     },
 	JobJoin = true,
-	JobId = "8078a7d3-eca4-4369-b7a1-3ef137024378"
+	JobId = "8b8e3ccf-5e1f-4033-85d6-75371b6b7d10"
 }
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
 if game.Players.LocalPlayer.Name ~= getgenv().rei.MainAccount then
