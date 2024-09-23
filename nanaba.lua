0797ffdf9890b966d6205be75d26efee9c1e21da
Dum <91747868+Dum1121@users.noreply.github.com>
2024-09-23 14:30:16 +0700

    Update nanaba.lua

--- nanaba.lua
+++ nanaba.lua
@@ -1,78 +1,29 @@
-repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
-getgenv().TargetUnitRoll = {
-    "Admiral Of Lava",
-    "Chief Of Ice",
-    "Chief Of Light",
-    "Strongest Swordsman",
-    "Maxed Qi Master",
-    "Purple Demon Berserker",
-    "Shinobi Form 3"
-}
-getgenv().notRollUnitTarget = false
-getgenv().UseSavePosition = false
-getgenv().GemRollUnit = 2500
-getgenv().Speed = {
-    ["Speed"] = 2,
-    ["Wave Active Speed"] = 12,
-}
-getgenv()["Black Screen"] = true
-getgenv()["Auto Leave Infinite"] = {
-    ["Auto Leave"] = true,
-    ["Method"] = {
-        ["Sell"] = true,
-        ["Leave"] = false,
-    },
-    ["Wave"] = 46
-}
-getgenv().Auto_Equip = {
-    ["Equip Best"] = true,
-    ["Custom Equip"] = {
-        ["Enabled"] = false,
-        ["Units"] = {
-            "Chief Of Lava",
-        },
-    },
-}
-getgenv().Portal = {
-    ["Enabled"] = false, 
-    ["Name Portal"] = "Demon Portal", -- support only 4 portal lunar and Demon and Ancient Dragon and Cursed Kingdom
-    ["Auto Get Portal"] = false, ---- support only portal Ancient Dragon and Cursed Kingdom
-    ["Rarity Portal"] = {
-        ["Rare"] = true,
-        ["Epic"] = true,
-        ["Legendary"] = false,
-        ["Mythical"] = false,
-    },
-    ["Unit"] = {
-        ["Use All Unit"] = true,
-        ["Custom Unit"] = {
-            "Admiral Of Lava",
-        }
-    },
-}
-getgenv().Webhook =  {
-    ["Webhook"] = true,
-    ["Url"] = "https://ptb.discord.com/api/webhooks/1281338105269325948/Z7lG5rScJHGBUHvw7sqzh_qiZYVd1k3-dg1YXgHqN3ugPtxxBtJk2IhP214Yn50ULyBn",
-    ["Roll Unit"] = true,
-    ["Story/Infinite"] = true,
-}
+for i,v in next, workspace:GetDescendants() do
+    pcall(function()
+        v.Transparency = 1
+    end)
+end
+for i,v in next, getnilinstances() do
+    pcall(function()
+        v.Transparency = 1
+        for i1,v1 in next, v:GetDescendants() do
+            v1.Transparency = 1
+        end
+    end)
+end
+a = workspace
+a.DescendantAdded:Connect(function(v)
+    pcall(function()
+        v.Transparency = 1
+    end)
+end)
 spawn(function()
         while wait(5) do
             pcall(function()
-                    setfpscap(3)
+                    setfpscap(10)
                 end)
         end
     end)
-spawn(function()
-        while wait() do
-            pcall(function()
-                    if game.PlaceId == 17490500437 then
-                        game.Players.LocalPlayer:Kick("? vào đây ăn cặc à?")
-                        game:GetService("TeleportService"):Teleport(17017769292)
-                        wait(20)
-                        game:Shutdown()
-                    end
-                end)
-        end
-    end)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaCat-KaitunAD.lua"))()
+getgenv().Configuration = true
+getgenv().key = '539b6039-ae10-4e05-802a-952d34bbd714'
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Xenon-Trash/Loader/main/Loader.lua'))()
