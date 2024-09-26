80bb7b7a9f1908e2b9911f52bca2f4a9041f1c2f
Dum <91747868+Dum1121@users.noreply.github.com>
2024-09-26 22:56:56 +0700

    Update nanaba.lua

--- nanaba.lua
+++ nanaba.lua
@@ -1,29 +1,23 @@
-for i,v in next, workspace:GetDescendants() do
-    pcall(function()
-        v.Transparency = 1
-    end)
-end
-for i,v in next, getnilinstances() do
-    pcall(function()
-        v.Transparency = 1
-        for i1,v1 in next, v:GetDescendants() do
-            v1.Transparency = 1
-        end
-    end)
-end
-a = workspace
-a.DescendantAdded:Connect(function(v)
-    pcall(function()
-        v.Transparency = 1
-    end)
-end)
-spawn(function()
-        while wait(5) do
-            pcall(function()
-                    setfpscap(10)
-                end)
-        end
-    end)
-getgenv().Configuration = true
-getgenv().key = '539b6039-ae10-4e05-802a-952d34bbd714'
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Xenon-Trash/Loader/main/Loader.lua'))()
+getgenv().RedeemAllCode = true
+getgenv().Config = {
+    ["Webhook"] = {
+        ["Mention"] = "",
+        ["Unit Summoned"] = true,
+        ["URL"] = "",
+    },
+    ["Misc"] = {
+        ["Max Camera Zoom"] = 40,
+    },
+    ["Summoner"] = {
+        ["Unselect if Summoned"] = true,
+            ["Roku (Dark)"] = true,
+        },
+        ["Auto Summon Standard"] = true,
+        ["Delete Rarity"] = {
+            ["Epic"] = true,
+            ["Legendary"] = false,
+            ["Rare"] = true,
+        },
+    }
+}
+loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()
