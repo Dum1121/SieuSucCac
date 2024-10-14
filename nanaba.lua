1eaa1f41e8ba6183a7271e606f9de69435fa9163
Dum <91747868+Dum1121@users.noreply.github.com>
2024-10-14 13:27:02 +0700

    Update nanaba.lua

--- nanaba.lua
+++ nanaba.lua
@@ -1,29 +1,64 @@
-for i,v in next, workspace:GetDescendants() do
-    pcall(function()
-        v.Transparency = 1
-    end)
-end
-for i,v in next, getnilinstances() do
-    pcall(function()
-        v.Transparency = 1
-        for i1,v1 in next, v:GetDescendants() do
-            v1.Transparency = 1
-        end
-    end)
-end
-a = workspace
-a.DescendantAdded:Connect(function(v)
-    pcall(function()
-        v.Transparency = 1
-    end)
-end)
-spawn(function()
-        while wait(5) do
-            pcall(function()
-                    setfpscap(10)
-                end)
-        end
-    end)
-getgenv().Configuration = true
-getgenv().key = '539b6039-ae10-4e05-802a-952d34bbd714'
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Xenon-Trash/Loader/main/Loader.lua'))()
+getgenv().Config = {
+    ["AutoSave"] = true,
+    ["Webhook"] = {
+        ["Minimum Chance"] = 1000000,
+        ["Pet Rolled"] = true,
+        ["URL"] = "https://ptb.discord.com/api/webhooks/1286776956389691442/JGyS5jS9ZPw2Hpr5XeBG_7IdkaYZC9YDnFfsfkx4o9_pxWB_xWiXnmUDydB0wrE4H-9O"
+    },
+    ["Consumer"] = {
+        ["Potion"] = {
+            ["Enable"] = true,
+            ["Potion"] = {
+                ["Coins Potion II"] = true,
+                ["Lucky Potion IV"] = true,
+                ["Lucky Potion V"] = true,
+                ["Lucky Potion II"] = true,
+                ["Instant Luck Potion I"] = true,
+                ["Coins Potion III"] = true,
+                ["Coins Potion V"] = true,
+                ["Instant Luck Potion II"] = true,
+                ["Lucky Potion I"] = true,
+                ["Lucky Potion III"] = true,
+                ["Coins Potion IV"] = true,
+                ["Rainbow Dice Potion"] = true,
+                ["Instant Luck Potion III"] = true,
+                ["Coins Potion I"] = true
+            }
+        },
+        ["Fruit"] = {
+            ["Enable"] = true,
+            ["Fruit"] = {
+                ["Watermelon"] = true,
+                ["Pineapple"] = true,
+                ["Rainbow"] = true,
+                ["Apple"] = true,
+                ["Orange"] = true,
+                ["Banana"] = true
+            }
+        }
+    },
+    ["Upgrader"] = {
+        ["Enable"] = true,
+        ["Keep Amount"] = 100
+    },
+    ["Crafter"] = {
+        ["Enable"] = true
+    },
+    ["Performance"] = {
+        ["Boost FPS"] = true,
+        ["Black Screen"] = true
+    },
+    ["Resource"] = {
+        ["Auto Hidden Gift"] = true,
+        ["Auto Farm"] = true,
+        ["Auto Dig"] = true
+    },
+    ["Buyer"] = {
+        ["Potion Vending"] = true
+    },
+    ["Interface"] = {
+        ["Auto Upgrade"] = true,
+        ["Auto Roll"] = true
+    }
+}
+loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()
